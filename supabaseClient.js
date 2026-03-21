c8b2225740d8a6fa7ea5a6bf9b7b3801a68079f5
thevincentlan <68439655+thevincentlan@users.noreply.github.com>
2026-03-21 00:00:55 -0400

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -5,6 +5,6 @@
  */
 
 const SUPABASE_URL = 'https://lcbylepuimqramwuqmxi.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY'; // Replace with your actual Public Anon Key from Supabase Settings -> API
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxjYnlsZXB1aW1xcmFtd3VxbXhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5Mjk4NDYsImV4cCI6MjA4OTUwNTg0Nn0.7kgdiw8fzUwniL7s2IVKe-5qJkeJficF-iSPNSLQ8r8'; // Replace with your actual Public Anon Key from Supabase Settings -> API
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
